0d95bc6ceffc6a4eb70fb50ea6e4a0e88f029de9
Valentin Fauchère <valentin.fauchere@gmail.com>
2019-10-30 17:37:51 +0100

    Valentin ajout des fichiers game over et index

---
+++ src/Controller/GameOverController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class GameOverController extends AbstractController
+{
+    public function gameOver()
+    {
+        return $this->twig->render('gameover/gameover.html.twig');
+    }
+}
