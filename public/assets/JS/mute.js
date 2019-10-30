1b22e61b30c431f06fdee718cb240442aa283eb5
Florentin Wilfried <florentinwilfried@gmail.com>
2019-10-31 00:40:11 +0100

    rajout du bouton mute

---
+++ public/assets/JS/mute.js
@@ -0,0 +1,13 @@
+var pause = document.querySelector(".pause");
+var audio = document.querySelector(".audio");
+
+function togglePlay() {
+    if (audio.paused) {
+        audio.play();
+        pause.innerHTML = "🔊";
+    } else {
+        audio.pause();
+        pause.innerHTML = "🔇";
+        pause.style.color = " #848484";
+    }
+}
